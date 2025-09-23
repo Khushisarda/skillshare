b26055c196059687a0a10d3c41e4155afd5ed0b5
KhushiSarda <khushisarda2005@gmail.com>
2025-09-23 19:32:35 +0530

    added add skill button

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
    #S path("", views.dashboard, name="dashboard"),
     path("edit-profile/", views.edit_profile, name="edit_profile"),
     path("add-project/", views.add_project, name="add_project"),
+    path("add_skill/",views.add_skill,name="add_skill"),
     path("send-message/<int:user_id>/", views.send_message, name="send_message"),
     path("mark-read/<int:notif_id>/", views.mark_notification_read, name="mark_notification_read"),
     path("dashboard/", views.dashboard, name="dashboard"),
